5d80d2971132cb3bca62944a6cafdb2e3e4e487d
ilyatitovich <ilyatitovich@gmail.com>
2025-01-16 20:50:02 +0100

    Add handle errors function

--- src/api/base-api.ts
+++ src/api/base-api.ts
@@ -1,8 +1,11 @@
+import { appRouter } from '@/app'
+import { showRequestResult } from '@/components'
 import { HTTPTransport } from '@/utils'
 
-export type APIResponse<T = unknown> = {
+export type APIResponse<T = unknown> = T
+
+export type APIError = {
   status: number
-  data: T
 }
 
 export abstract class BaseAPI {
@@ -10,5 +13,32 @@ export abstract class BaseAPI {
 
   constructor() {
     this.http = new HTTPTransport('https://ya-praktikum.tech/api/v2')
+  }
+
+  handleError(error: APIError, message: string, callback: () => void): void {
+    const { status } = error
+
+    switch (status) {
+      case 400:
+        showRequestResult(false, message)
+        break
+      case 401:
+        showRequestResult(false, 'Вы не авторизованы')
+        break
+      case 409:
+        showRequestResult(
+          false,
+          'Пользователь с таким логином или почтой уже зарегистрирован'
+        )
+        break
+      case 500:
+        appRouter.go('/500')
+        break
+      default:
+        showRequestResult(false, 'Произошла ошибка')
+        break
+    }
+
+    callback()
   }
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export * from './auth-api'
 export * from './user-api'
 export * from './chats-api'
+export * from './base-api'
