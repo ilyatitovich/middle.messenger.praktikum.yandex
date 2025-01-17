23053f245a589edf5491631bc0c4a0d77f5a8518
ilyatitovich <ilyatitovich@gmail.com>
2025-01-17 11:22:26 +0100

    Upadte chat message

--- src/components/chat/chat-message/chat-message.ts
+++ src/components/chat/chat-message/chat-message.ts
@@ -1,23 +1,25 @@
 import './chat-message.css'
 
-import { Block, type BlockProps } from '@/core/block'
-import { getTemplate, type Message } from '@/utils'
+import { Block, type BlockProps, type MessageResponse } from '@/core'
+import { userStore } from '@/stores'
+import { getTemplate } from '@/utils'
 
 import MessageTemplate from './chat-message.hbs?raw'
 
-type ChatMessageProps = BlockProps & {
-  message: Message
+export type ChatMessageProps = BlockProps & {
+  message: MessageResponse
 }
 
 export class ChatMessage extends Block<ChatMessageProps> {
   constructor(props: ChatMessageProps) {
+    const { user } = userStore.get()
     super('div', {
       ...props,
-      className: `message ${props.message.sender === 'me' ? 'right' : 'left'}`
+      className: `message ${props.message.user_id === user!.id ? 'right' : 'left'}`
     })
   }
 
   protected render(): string {
-    return getTemplate(MessageTemplate, { text: this.props.message.text })
+    return getTemplate(MessageTemplate, { text: this.props.message.content })
   }
 }
