49d64e1238124f59977343bbab879824fec280c4
ilyatitovich <ilyatitovich@gmail.com>
2025-01-20 19:31:18 +0100

    Add add chat users to chat description

--- src/components/chat/current-chat-data/current-chat-data.ts
+++ src/components/chat/current-chat-data/current-chat-data.ts
@@ -1,4 +1,5 @@
 import { Block, type BlockProps } from '@/core'
+import { type ChatUsersState, chatUsersStore, userStore } from '@/stores'
 import { getFilePath, getTemplate } from '@/utils'
 
 import CurrentChatDataTemplate from './current-chat-data.hbs?raw'
@@ -7,18 +8,42 @@ export type CurrentChatDataProps = BlockProps & {
   id?: number
   avatar?: string | null
   title?: string
+  userNames?: string
 }
 
 export class CurrentChatData extends Block<CurrentChatDataProps> {
   constructor(props: CurrentChatDataProps) {
     super('div', { ...props, className: 'chat-main__header-left' })
+
+    this.storeUnsubscribe = chatUsersStore.subscribe(state => {
+      const { user: currentUser } = userStore.get()
+      const { users } = state as ChatUsersState
+
+      const userNames = [...users]
+        .reduce<string[]>((acc, user) => {
+          const userName =
+            user.id === currentUser!.id ? 'Вы' : user.display_name || user.login
+
+          if (user.id === currentUser!.id) {
+            acc.unshift(userName!)
+          } else {
+            acc.push(userName!)
+          }
+
+          return acc
+        }, [])
+        .join(', ')
+
+      this.setProps<CurrentChatDataProps>({ userNames })
+    })
   }
 
   protected render(): string {
-    const { id, avatar, title } = this.props
+    const { id, avatar, title, userNames } = this.props
     return getTemplate(CurrentChatDataTemplate, {
       avatar: avatar ? getFilePath(avatar) : `https://robohash.org/${id}`,
-      title
+      title,
+      userNames
     })
   }
 }
