f8639cb481cbfc77b9f3446780cf829aa76be2e3
ilyatitovich <ilyatitovich@gmail.com>
2025-01-28 12:49:19 +0100

    Add mocka and chai

---
+++ mocha-setup.js
@@ -0,0 +1,17 @@
+import { JSDOM } from 'jsdom'
+import { register } from 'node:module'
+import { pathToFileURL } from 'node:url'
+
+register('ts-node/esm', pathToFileURL('./'))
+
+const { window } = new JSDOM('<!doctype html><html><body></body></html>', {
+  url: 'https://y-chat.com'
+})
+global.window = window
+global.document = window.document
+global.HTMLElement = window.HTMLElement
+
+// Mocking requestAnimationFrame
+global.requestAnimationFrame = callback => {
+  return setTimeout(callback, 0)
+}
